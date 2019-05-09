ea2380358aebaef912addb49d52668712238c675
hong <hong@501-016>
2019-05-09 15:48:05 +0900

    notice pageutil

---
+++ SiBal/src/main/java/sb/com/controller/NoticeController.java
@@ -0,0 +1,57 @@
+package sb.com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/notice")
+public class NoticeController {
+	//공지사항 리스트
+	@RequestMapping("/List")
+	public void noticeList() {
+		
+	}
+	
+	//공지사항 상세보기
+	@RequestMapping("/View")
+	public void noticeView() {
+		
+	}
+	
+	//공지사항 글쓰기폼
+	@RequestMapping("/writeForm")
+	public void noticeWriteForm() {
+		
+	}
+	
+	//공지사항 글쓰기 처리
+	@RequestMapping("/writeProc")
+	public void noticeWriteProc() {
+		
+	}
+	
+	//조회수 처리
+	@RequestMapping("/hitProc")
+	public void noticeHitProc() {
+		
+	}
+	
+	//공지사항 수정 폼
+	@RequestMapping("/modifyForm")
+	public void noticeModifyForm() {
+		
+	}
+	
+	//공지사항 글쓰기 처리
+	@RequestMapping("/modifyProc")
+	public void noticeModifyProc() {
+		
+	}
+	
+	//글삭제 처리
+	@RequestMapping("/deleteProc")
+	public void noticeDeleteProc(){
+		
+	}
+	
+}

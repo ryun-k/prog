13b2a5ef0e7e4fae2d19375a9cc56eeae9d5608b
hong <hong@501-016>
2019-05-13 16:00:58 +0900

    공지사항 리스트 출력완료

--- SiBal/src/main/java/sb/com/service/NoticeService.java
+++ SiBal/src/main/java/sb/com/service/NoticeService.java
@@ -9,5 +9,29 @@ import sb.com.util.PageUtil;
 import sb.com.vo.NoticeVo;
 
 public class NoticeService {
+	@Autowired
+	private NoticeDAO nDAO;
 	
+	//페이징처리 실행함수
+	public PageUtil getPageInfo(int nowPage) {
+		//총 데이터 개수 구하자
+		int totalCount = nDAO.getTotalCount();
+		
+		PageUtil pageInfo = new PageUtil(nowPage,totalCount,5,5);
+		return pageInfo;
+	}
+	
+	//목록을 구하는 함수
+	public ArrayList getBoardList(PageUtil  pInfo){
+
+		int start = (pInfo.getNowPage()-1) * pInfo.getListCount()+1;
+		int end   = start + pInfo.getListCount() -1;
+		
+		NoticeVo vo = new NoticeVo();
+		vo.setStart(start);
+		vo.setEnd(end);
+		
+		ArrayList list = nDAO.getNoticeList(vo);
+		return list;
+	}
 }

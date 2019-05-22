0bdf39cabbb1bbc46a53439f027efca18c08f76d
hong <hong@501-016>
2019-05-22 11:35:39 +0900

    main

---
+++ SiBal/src/main/java/sb/com/controller/MaininfoController.java
@@ -0,0 +1,15 @@
+package sb.com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/maininfo")
+public class MaininfoController {
+	
+	@RequestMapping("/trip")
+	public void trip() {
+		
+	}
+	
+}
